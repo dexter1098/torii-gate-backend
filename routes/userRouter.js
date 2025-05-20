8bafc2afe5394c3e519a38b4fa571c9b626b6e68
dexter1098 <emmadexter50@gmail.com>
2025-05-20 14:27:34 +0100

    feat: added verification auth func

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,10 +3,12 @@ const {
   handleRegister,
   handleVerifyEmail,
   handleLogin,
+  resendVerificationEmail,
 } = require("../controllers/userController");
 
 router.post("/register", handleRegister);
 router.post("/verify-email/:token", handleVerifyEmail);
 router.post("/login", handleLogin);
+router.post("/resend-email/:token", resendVerificationEmail)
 
 module.exports = router;
