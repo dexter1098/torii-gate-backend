86c7e821a0fe052259528db996d30e09775db5cf
dexter1098 <emmadexter50@gmail.com>
2025-06-03 13:08:21 +0100

    feat: created delete property

--- routes/PropertyRouter.js
+++ routes/PropertyRouter.js
@@ -6,18 +6,34 @@ const {
   updatePropertyAvailability,
   getAllProperties,
   getAProperty,
+  deleteProperty,
 } = require("../controllers/PropertyController");
-const {isloggedin, requirePermissions} = require('../middleware/auth')
+const { isloggedin, requirePermissions } = require("../middleware/auth");
 
-router.post('/', isloggedin, requirePermissions('landlord'), createProperty)
-router.get('/landlord', isloggedin, requirePermissions('landlord'), getLandlordProperties)
-router.patch('/landlord/:propertyId', isloggedin, requirePermissions('landlord'), updatePropertyAvailability)
+router.post("/", isloggedin, requirePermissions("landlord"), createProperty);
+router.get(
+  "/landlord",
+  isloggedin,
+  requirePermissions("landlord"),
+  getLandlordProperties
+);
+router.patch(
+  "/landlord/:propertyId",
+  isloggedin,
+  requirePermissions("landlord"),
+  updatePropertyAvailability
+);
+router.delete(
+  "/landlord/:propertyId",
+  isloggedin,
+  requirePermissions("landlord"),
+  deleteProperty
+);
 
 // tenant
-router.get('/', isloggedin, getAllProperties)
-router.get('/:propertyId', isloggedin, getAProperty)
+router.get("/", isloggedin, getAllProperties);
+router.get("/:propertyId", isloggedin, getAProperty);
 
-
-
+//delete
 
 module.exports = router;
